0ca2dfea2cd428eb2fce7d908f58e6efa6e21fb9
codenicho <nicholascameron45@gmail.com>
2022-07-12 14:23:10 -0500

    Added stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed App Text.
+          Changed App Text. GAIAN!!
         </p>
         <a
           className="App-link"
