1c58426513b2db18035e535be09cb29616c2a577
codenicho <nicholascameron45@gmail.com>
2022-07-12 14:11:45 -0500

    Changed paragraph text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yes we made it fo real
+          Changed App Text.
           
         </p>
         <a
