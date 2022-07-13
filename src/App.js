1b9f150ba8c023c792630ebbcb1fd1b7eb28bef5
codenicho <nicholascameron45@gmail.com>
2022-07-12 22:49:50 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LA LA LA pp Text. GAIAN!!
+          TEXT CHANGED
         </p>
         <a
           className="App-link"
